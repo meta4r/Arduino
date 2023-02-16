fa7f840fd605ae3ae1fc4e39b36fd8c8778384fb
meta4r <alalemns@gmail.com>
2023-02-16 23:48:50 +0200

    Create Motor Control.ino

---
+++ Motor Control.ino
@@ -0,0 +1,27 @@
+int in1 = 9;
+int in2 = 10;
+int pwm = 6;
+
+void setup() {
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(pwm, OUTPUT);
+}
+
+void loop() {
+  analogWrite(pwm, 150);
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  delay(2000);
+  
+  analogWrite(pwm, 0);
+  delay(1000);
+  
+  analogWrite(pwm, 200);
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  delay(2000);
+  
+  analogWrite(pwm, 0);
+  delay(1000);
+}
