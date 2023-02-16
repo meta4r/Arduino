2ef923f7a4d769d2f1ec9a435c9ecfddaadb0fa4
meta4r <alalemns@gmail.com>
2023-02-16 23:53:02 +0200

    Create LDR.ino

---
+++ LDR.ino
@@ -0,0 +1,19 @@
+const int LDR_PIN = A0;
+const int LED_PIN = 2;
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  int ldr_value = analogRead(LDR_PIN);
+  Serial.print("LDR value: ");
+  Serial.println(ldr_value);
+  if (ldr_value > 500) {
+    digitalWrite(LED_PIN, HIGH);
+  } else {
+    digitalWrite(LED_PIN, LOW);
+  }
+  delay(1000);
+}
